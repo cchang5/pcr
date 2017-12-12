dc64e9bff67fac20a039063d885e8578faa43856
Jason Chang <pyo0220@gmail.com>
2017-12-11 16:34:40 -0800

    add 3 pt fit

--- pcr_lib.py
+++ pcr_lib.py
@@ -94,10 +94,13 @@ class fitter_class():
         for k in data.keys():
             cla = k.split('_')[0]
             x[k] = dict()
-            x[k]['t'] = trange[cla]
-            if cla in ['gV','dgV']:
-                x[k]['T'] = int(k.split('_')[2][1:])
+            if cla in ['nucleon','dnucleon']:
+                x[k]['t'] = trange[cla]
+            elif cla in ['gV','dgV']:
+                T = int(k.split('_')[2][1:])
+                x[k]['T'] = T
                 x[k]['cur'] = cla.split('d')[-1]
+                x[k]['t'] = trange['%s_T%s' %(cla,T)]
             x[k]['q'] = int(k[-1])
             x[k]['snk'] = k.split('_')[1][0].lower()
             x[k]['src'] = k.split('_')[1][1].lower()
@@ -116,8 +119,8 @@ class fitter_class():
         pdict = dict()
         pdict['nucleon'] = ['E','Z']
         pdict['dnucleon'] = ['E','Z','Y']
-        pdict['gV'] = ['E','Z','G']
-        pdict['dgV'] = ['E','Z','G','Y','F']
+        pdict['gV'] = ['E','Z','V']
+        pdict['dgV'] = ['E','Z','V','Y','U']
         p = dict()
         plist = np.array([])
         for c in self.clist:
@@ -129,7 +132,7 @@ class fitter_class():
                 smears = ''
                 smears = [i.lower() for i in np.unique(list(smears.join(self.snk_src)))]
                 # for energy and matrix elements
-                if k[0] in ['E','G','F']:
+                if k[0] in ['E','V','U']:
                     p[k] = ap[k]
                 # for smearing dependent quantities (overlaps)
                 elif k[2] in smears:
@@ -144,10 +147,10 @@ class fitter_class():
         return En
     def Z(self,p,q,n,s):
         return p['Z%s%s_q%s' %(str(n),str(s),str(q))]
-    def dZ(self,p,q,ns):
+    def dZ(self,p,q,n,s):
         return p['Y%s%s_q%s' %(str(n),str(s),str(q))]
     def c2pt(self,T,En,Zn_snk,Zn_src):
-        return Zn_snk*Zn_src*np.exp(-En*T) / (2.*En)
+        return Zn_snk*Zn_src*np.exp(-En*T)
     def twopt(self,x,p):
         # unpack x
         T = x['t']
@@ -158,8 +161,8 @@ class fitter_class():
         r = 0
         for n in range(self.nstate):
             En = self.E(p,q,n)
-            Zn_snk = self.Z(p,n,q,snk)
-            Zn_src = self.Z(p,n,q,src)
+            Zn_snk = self.Z(p,q,n,snk)
+            Zn_src = self.Z(p,q,n,src)
             r += self.c2pt(T,En,Zn_snk,Zn_src)
         return r
     def dtwopt(self,x,p):
@@ -172,20 +175,32 @@ class fitter_class():
         r = 0
         for n in range(self.nstate):
             En = self.E(p,q,n)
-            Zn_snk = self.Z(p,n,q,snk)
-            Zn_src = self.Z(p,n,q,src)
-            dZn_snk = self.dZ(p,n,q,snk)
-            dZn_src = self.dZ(p,n,q,src)
+            Zn_snk = self.Z(p,q,n,snk)
+            Zn_src = self.Z(p,q,n,src)
+            dZn_snk = self.dZ(p,q,n,snk)
+            dZn_src = self.dZ(p,q,n,src)
             c2ptn = self.c2pt(T,En,Zn_snk,Zn_src)
             r += c2ptn * (dZn_snk/Zn_snk + dZn_src/Zn_src - 0.5/En**2 - 0.5*T/En)
         return r
     # three point fit functions
-    def G(self,p,m,n,q,cur):
-        return p['G%s%s%s_q%s' %(str(cur),str(m),str(n),str(q))]
-    def dG(self,p,n,m,q,cur):
-        return p['F%s%s%s_q%s' %(str(cur),str(m),str(n),str(q))]
+    def G(self,p,q,m,n,cur):
+        if n > m:
+            if cur in ['gV']:
+                return p['V%s%s_q%s' %(str(n),str(m),str(q))]
+            else:
+                return 0
+        else:
+            if cur in ['gV']:
+                return p['V%s%s_q%s' %(str(m),str(n),str(q))]
+            else:
+                return 0
+    def dG(self,p,q,m,n,cur):
+        if cur in ['gV']:
+            return p['U%s%s_q%s' %(str(m),str(n),str(q))]
+        else:
+            return 0
     def c3pt(self,t,T,Em,En,Zm_snk,Zn_src,Gmn):
-        return Zm_snk*Gmn*Zn_src*np.exp(-Em*T-(En-Em)*t)/(4.*Em*En)
+        return Zm_snk*Gmn*Zn_src*np.exp(-Em*T-(En-Em)*t)
     def threept(self,x,p):
         # unpack x
         t = x['t']
@@ -198,12 +213,13 @@ class fitter_class():
         r = 0
         for m in range(self.nstate):
             for n in range(self.nstate):
-                Em = self.E(p,q,m)
-                En = self.E(p,0,n)
-                Zm_snk = self.Z(p,m,0,snk)
-                Zn_src = self.Z(p,n,q,src)
-                Gmn = self.G(p,m,n,q,cur)
+                Em = self.E(p,0,m)
+                En = self.E(p,q,n)
+                Zm_snk = self.Z(p,0,m,snk)
+                Zn_src = self.Z(p,q,n,src)
+                Gmn = self.G(p,q,m,n,cur)
                 r += self.c3pt(t,T,Em,En,Zm_snk,Zn_src,Gmn)
+        return r
     def dthreept(self,x,p):
         # unpack x
         t = x['t']
@@ -216,15 +232,16 @@ class fitter_class():
         r = 0
         for m in range(self.nstate):
             for n in range(self.nstate):
-                Em = self.E(p,q,m)
-                En = self.E(p,0,n)
-                Zm_snk = self.Z(p,m,0,snk)
-                Zn_src = self.Z(p,n,q,src)
-                dZn_src = self.dZ(p,n,q,src)
-                Gmn = self.G(p,m,n,q,cur)
-                dGmn = self.dG(p,m,n,q,cur)
+                Em = self.E(p,0,m)
+                En = self.E(p,q,n)
+                Zm_snk = self.Z(p,0,m,snk)
+                Zn_src = self.Z(p,q,n,src)
+                dZn_src = self.dZ(p,q,n,src)
+                Gmn = self.G(p,q,m,n,cur)
+                dGmn = self.dG(p,q,m,n,cur)
                 c3ptmn = self.c3pt(t,T,Em,En,Zm_snk,Zn_src,Gmn)
                 r += c3ptmn * (dGmn/Gmn + dZn_src/Zn_src - 0.5/Em**2 - 0.5*t/Em)
+        return r
     # simultaneous fits
     def fit_function(self,x,p):
         r = dict()
